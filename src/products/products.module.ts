fe57aa80e2066d2438de66e130c457fb009ea1e8
Pietro Amorim <seu-email@exemplo.com>
2025-09-03 11:29:07 -0300

    02.09.25

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ProductEntity } from './entities/product.entity'; // Importe a entidade ProductEntity
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ProductEntity])], // Registra a entidade no módulo
   controllers: [ProductsController],
   providers: [ProductsService]
 })
-export class ProductsModule {}
+
+export class ProductsModule {} 
